b4ef457f866c892bcec9dff2d3262ccbb4edc1df
Leonardo Rimes <rimesleo@gmail.com>
2023-06-19 10:46:19 -0300

    adicionando fetch dos filmes na api

--- MoviesLib/src/App.jsx
+++ MoviesLib/src/App.jsx
@@ -1,20 +1,18 @@
 import './App.css'
-import { Link, Outlet } from 'react-router-dom'
+import {  Outlet } from 'react-router-dom'
+
+
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <div className='app'>
-      <nav id="navbar">
-        <h2>
-          <h2><Link to="/">MoviesLib</Link></h2>
-          <Link to="/movie/1">Movie</Link>
-          <Link to="/search">Search</Link>
-        </h2>
-      </nav>
+        <Navbar />
       <h2> Movies Lib </h2>
        <Outlet />
+
     </div>
   )
 }
