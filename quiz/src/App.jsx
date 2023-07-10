72ee6f4264bd7394eb91ec0999ed178dd0570eff
Leonardo Rimes <rimesleo@gmail.com>
2023-07-10 10:50:38 -0300

    Adicionando projeto quizApp

---
+++ quiz/src/App.jsx
@@ -0,0 +1,27 @@
+// react, componentes, estáticos
+import { useContext, useEffect } from 'react'
+
+import Welcome from './components/Welcome'
+import Question from './components/Question'
+import {QuizContext} from './context/quiz'
+
+import './App.css'
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+  
+  useEffect(() => {
+    dispatch({type: "REORDER_QUESTIONS"})
+  })
+
+  return (
+    <div className="App">
+        <h1> Quiz App</h1>
+        {quizState.gameStage === "Start" && <Welcome />}
+        {quizState.gameStage === "Playing" && <Question />}
+
+    </div>
+  )
+}
+
+export default App
